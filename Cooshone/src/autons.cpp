a6c8da1d309fecb9d8235411fd582b96b9a5cda6
Gabko42069 <gabkohanim@gmail>
2024-02-08 17:22:13 -0800

    nikita

--- Cooshone/src/autons.cpp
+++ Cooshone/src/autons.cpp
@@ -4,20 +4,47 @@
 #include "pros/motors.h"
 
 
-
+//botton right
 void auton1()
 {
   chassis.setPose(0, 0, 45);
   moveIn(35, 2500, false, false);
   turnDegree(0,true);
-  // moveIn(20, 2500, false, true);
-  // turnDegree(45,true);
-  // moveIn(20, 2500, false, true);
-  // turnDegree(45,true);
-  // moveIn(40, 2500, false, true)
+  moveIn(20, 2500, false, true);
+  turnDegree(45,true);
+  moveIn(20, 2500, false, true);
+  turnDegree(45,true);
+  moveIn(40, 2500, false, true);
 
 
 }
+
+//bottom left 
+
+void auton2() {
+    chassis.setPose(0, 0, 45);
+    moveIn(35, 2500, false, false);
+    turnDegree(180,true);
+    moveIn(50, 2500, false, true);
+    
+
+
+}
+
+
+
+//top left 
+void auton3() {
+    chassis.setPose(0, 0, 45);
+
+}
+
+//top right 
+void auton4() {
+      chassis.setPose(0, 0, 45);
+
+}
+
 
 void autonDownRight() {
     chassis.setPose(60, -40, 180);
